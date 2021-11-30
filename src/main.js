10dd6713c19adaca391144e019446e9bbb097de9
18896605194 <1585628173@qq.com>
2021-11-30 16:52:57 +0800

    登录界面搭建

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 
+import './styles/index.scss'// 导入全局样式
 const app = createApp(App)
 installElementPlus(app)
 app.use(store).use(router).mount('#app')
